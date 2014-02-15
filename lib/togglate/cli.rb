df63dda0f1d700a960cf6317faa08bcb92a1a903
kyoendo <postagie@gmail.com>
2014-02-15 19:07:02 +0900

    add option for wrap_exceptions

--- lib/togglate/cli.rb
+++ lib/togglate/cli.rb
@@ -3,12 +3,13 @@ require "thor"
 module Togglate
   class CLI < Thor
     desc "create FILE", "Create a base file for translation from a original file"
-    option :code, aliases:'-c', default:true
+    option :code, aliases:'-c', default:true, type: :boolean
     option :method, aliases:'-m', default:'hover', desc:"any of 'hover' or 'toggle'"
     option :target, type: :string
     option :show_text, aliases:'-s', type: :string
     option :hide_text, aliases:'-h', type: :string
     option :wrapper, type: :array
+    option :wrap_exceptions, type: :array
     def create(file)
       opts = options.inject({}) { |h, (k,v)| h[k.intern] = v; h } # symbolize keys
       if wrapper = options[:wrapper]
